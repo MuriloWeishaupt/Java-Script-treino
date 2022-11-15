16485904956e9f58505b289a90084fb3ee80b276
Murilo Weishaupt de Camargo <muriloweishaupt@gmail.com>
2022-11-15 09:14:44 -0200

    Aula10 e aula11

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 40
+console.log (`A velocidade do seu veículo é de ${vel} Km/h`)
+console.log ('Dirija sempre com sinto de segurança')
+    if (vel> 60) {
+        console.log ('Você foi multado por excesso de velocidade')
+    }
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'França'
+console.log (`Seu país de origem é ${país}`)
+    if (país != 'Brasil') {
+        console.log ('Você é estrangeiro!')
+    } else {
+        console.log ('Você é Brasileiro!')
+    }
