cb7255bed78d9031f70f2e8f42994e81dfe97a92
Murilo Weishaupt de Camargo <muriloweishaupt@gmail.com>
2022-11-19 15:48:33 -0200

    Exercícios

---
+++ ex016/script.js
@@ -0,0 +1,12 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fAno = window.document.getElementById('txtano')
+    var res = window.document.getElementById('res')
+    var radsex = window.aler.getElementById('tradio')
+    if (fAno.value.lenght == 0 || fAno.value > ano) {
+        window.alert ('[ERRO] verifique os dados e tente novamente!')
+    } else {
+        
+    }
+    }
