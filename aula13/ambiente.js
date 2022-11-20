5bcb2e5a867f886aa81dc344ce2f2473750bbb26
Murilo Weishaupt de Camargo <muriloweishaupt@gmail.com>
2022-11-20 18:34:25 -0200

    Estrutura "While"  "Do While"

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,5 +1,17 @@
+// Estrutura de repetição com Teste lógico no Final
+
 var c = 1
 do {
     console.log(`Passo ${c}`)
     c++
-} while {c <= 100}
+} while (c <=100)
+
+
+
+// Estrutura de repetição com teste lógico no Começo
+
+var c = 1
+while(c <= 100) {
+    console.log(`Passo ${c}`)
+    c++
+}
